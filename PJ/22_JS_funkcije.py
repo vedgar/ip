5fe28e6f8a6543cb5b41977912340358ad266d13
Veky <veky@math.hr>
2020-05-30 21:29:56 +0200

    JS

---
+++ PJ/22_JS_funkcije.py
@@ -0,0 +1,91 @@
+"""Leksički i sintaksni analizator za JavaScriptove funkcije.
+Kolokvij 19. siječnja 2012. (Puljić)"""
+
+
+from pj import *
+
+
+class T(TipoviTokena):
+    FUNCTION, VAR, NAREDBA, KOMENTAR = 'function', 'var', 'naredba', '//'
+    O_OTV, O_ZATV, V_OTV, V_ZATV, KOSACRTA, ZAREZ, TOČKAZAREZ = '(){}/,;'
+    IME = TipTokena()
+
+
+def js(lex):
+    for znak in lex:
+        if znak.isspace(): lex.zanemari()
+        elif znak.isalpha() or znak == '$':
+            lex.zvijezda(identifikator)
+            yield lex.literal(T.IME)
+        elif znak == '/':
+            if lex >> '/':
+                lex.pročitaj_do('\n')
+                yield lex.token(T.KOMENTAR)
+            else: yield lex.token(T.KOSACRTA)
+        else: yield lex.literal(T)
+
+
+### Beskontekstna gramatika
+# start -> fun | start fun
+# fun -> FUNCTION IME O_OTV argumenti? O_ZATV V_OTV KOMENTAR* nar V_ZATV
+# argumenti -> VAR IME ZAREZ argumenti | VAR IME
+# nar -> NAREDBA TOČKAZAREZ nar | NAREDBA KOMENTAR+ nar | NAREDBA | ''
+
+class P(Parser):
+    lexer = js
+
+    def funkcija(self):
+        self.pročitaj(T.FUNCTION)
+        ime = self.pročitaj(T.IME)
+        self.pročitaj(T.O_OTV)
+        argumenti = []
+        if self >= T.VAR:
+            argumenti = [self.argument()]
+            while self >> T.ZAREZ: argumenti.append(self.argument())
+        self.pročitaj(T.O_ZATV)
+        return Funkcija(ime, argumenti, self.tijelo())
+
+    def tijelo(self):
+        self.pročitaj(T.V_OTV)
+        while self >> T.KOMENTAR: pass
+        naredbe = []
+        while not self >> T.V_ZATV:
+            naredbe.append(self.naredba())
+            if self >> T.TOČKAZAREZ: pass
+            elif self >> T.KOMENTAR:
+                while self >> T.KOMENTAR: pass
+            elif self >> T.V_ZATV: break
+            else: self.greška()
+        return naredbe
+
+    def start(self):
+        funkcije = [self.funkcija()]
+        while not self >> KRAJ: funkcije.append(self.funkcija())
+        return Program(funkcije)
+
+    def argument(self):
+        self.pročitaj(T.VAR)
+        return self.pročitaj(T.IME)
+
+    def naredba(self): return self.pročitaj(T.NAREDBA)
+
+
+### AST
+# Funkcija: ime:IME argumenti:[IME] tijelo:[NAREDBA]
+# Program: funkcije:[Funkcija]
+
+class Funkcija(AST('ime argumenti tijelo')): pass
+class Program(AST('funkcije')): pass
+
+
+prikaz(P('''\
+    function ime (var x, var y, var z) {
+        //neke naredbe odvojene s ; ili komentarima
+        naredba; naredba //kom 
+        naredba
+    }
+    function ništa(){}
+    function $trivijalna(var hmmm){naredba// komm/
+    //
+    }
+'''), 3)
