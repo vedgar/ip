713524e535049ae58d027b0df5b44fc5d6248f1d
Marko Horvat <artakserkso@gmail.com>
2021-06-05 22:08:52 +0200

    Reverted "typo" "fix".

--- PJ/18_ar_Q_statyping.py
+++ PJ/18_ar_Q_statyping.py
@@ -42,7 +42,7 @@ class Tip(enum.Enum):
 ### Beskontekstna gramatika
 # start -> NOVIRED? niz_naredbi NOVIRED?
 # niz_naredbi -> naredba | naredba NOVIRED niz_naredbi
-# naredba -> UPIT izraz | (NAT|INT|RAT) IME JEDNAKO izraz
+# naredba -> UPIT izraz | (NAT|INT|RAT)? IME JEDNAKO izraz
 # izraz -> član | izraz (PLUS|MINUS) član
 # član -> faktor | član (PUTA|KROZ|DIV|MOD) faktor
 # faktor -> baza | baza NA faktor | MINUS faktor
